b8e397c41b03880ccf008ae431ea030fb771db04
Ermisgc <1464817557@qq.com>
2025-01-19 15:31:57 +0800

    a normal git push

--- src/GcSpdlog/sink.cpp
+++ src/GcSpdlog/sink.cpp
@@ -1,13 +1,28 @@
 #include "./GcSpdlog/sink.h"
 namespace gcspdlog{
-Sink::Sink(Level level, const Formatter::ptr Formatter):m_level(level), m_formatter(Formatter){
+Sink::Sink(Level level, const Formatter::ptr Formatter):m_level(level), m_formatter(Formatter){}
 
-}
 Sink::~Sink(){}
 
-void Sink::log(Level level, const std::string & msg){
-    //std::cout << "sink"
+void Sink::log(LogMsg::ptr msg){
+    if(msg->level < this->m_level) return;
+    std::cout << m_formatter->format(msg) << std::endl;
 }
 
+void Sink::setFormatter(const Formatter::ptr formatter){
+    if(formatter != nullptr) this->m_formatter = formatter;
+}
+
+FileSink::FileSink(Level Level, const Formatter::ptr formatter,const std::string & file):file_name(file), Sink(Level, formatter){
+
+}
+
+FileSink::~FileSink(){
+
+}
+        
+void FileSink::log(LogMsg::ptr msg) {
+
+}
 
 }
--- src/include/GcSpdlog/sink.h
+++ src/include/GcSpdlog/sink.h
@@ -2,15 +2,27 @@
 #include "formatter.h"
 
 namespace gcspdlog{
+    // Sink corresponds to Appender in Log4J, which depends the logging method in different destination.
+    // this could be used directly(as a stdoutSink), or used by deriving a new class for custom.
     class Sink{
         Formatter::ptr m_formatter;
         Level m_level;
     public:
         using ptr = std::shared_ptr<Sink>;
-        Sink(Level level, const Formatter::ptr Formatter);
+        Sink(Level level, const Formatter::ptr formatter);
+        void setFormatter(const Formatter::ptr formatter);
         virtual ~Sink();
 
         virtual void log(LogMsg::ptr msg);
-        
     };
+
+    class FileSink: Sink{
+        std::string file_name;
+    public:
+        using ptr = std::shared_ptr<FileSink>;
+        FileSink(Level Level, const Formatter::ptr formatter,const std::string & file);
+        ~FileSink();
+
+        void log(LogMsg::ptr msg) override;
+    }
 }
